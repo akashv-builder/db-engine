69a14d15d683878417829f1ab2cddc4a00bd7595
akash <akash.verma926@gmail.com>
2018-02-19 19:06:54 +0530

    added more test cases

--- src/test/java/com/dbEngine/Test/TestQueryParameter.java
+++ src/test/java/com/dbEngine/Test/TestQueryParameter.java
@@ -1,5 +1,8 @@
 package com.dbEngine.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,7 +14,7 @@ import com.dbEngine.queryParameter.QueryParameters;
 @RunWith(JUnitPlatform.class)
 class TestQueryParameter {
 	QueryParameters qp=null;
-	String query="select * from ipl.csv where id>100 ;";
+	
 	@BeforeEach
     void beforeEach() {
     	
@@ -24,16 +27,85 @@ class TestQueryParameter {
     @Test
     void firstTest() {
     	
-    	assertEquals("select * from ipl.csv where id>100 and city='Banglore",qp.getQuery());
+    	assertEquals("select * from ipl.csv where id>100 ;",qp.getQuery());
         System.out.println("First test method");
     }
  
     @Test
     void secondTest() {
+    	String query="select * from ipl.csv where id>100 ;";
     	String []arr=  {"select", "*", "from", "ipl.csv", "where", "id>100", ";"};
-    	assertEquals(arr,qp.tokenizeQuery(query));
-        System.out.println("First test method");
+    	assertArrayEquals(arr,qp.tokenizeQuery(query));
+        System.out.println("Second test method");
     }
+    
+    @Test
+    void thirdTest() {
+    	String query="select * from ipl.csv where id>100 ;";
+    	assertEquals("select * from ipl.csv",qp.setBasePart(query));
+        System.out.println("Third test method");
+    }
+    @Test
+    void fourthTest() {
+    	String query="select * from ipl.csv where id>100 ;";
+    	assertEquals("id>100 ;",qp.setAfterWherePart(query));
+        System.out.println("Fourth test method");
+    }
+    @Test
+    void fifthTest() {
+    	String query="select * from ipl.csv where id>100 ;";
+    	ArrayList<String> al= new ArrayList<String>();
+    	al.add("ipl.csv");
+    	assertEquals(al,qp.setFileName(query));
+        System.out.println("Fifth test method");
+    }
+    @Test
+    void sixthTest() {
+    	String query="select * from ipl.csv where id>100 and city='Banglore' ;";
+    	ArrayList<String> al= new ArrayList<String>();
+    	al.add("id>100");
+    	al.add("city='Banglore'");
+    	assertEquals(al,qp.setConditions(query));
+        System.out.println("Sixth test method");
+    }
+    void seventhTest() {
+    	String query="select * from ipl.csv where id>100 and city='Banglore' ;";
+    	ArrayList<String> al= new ArrayList<String>();
+    	al.add(">");
+    	al.add("=");
+    	assertEquals(al,qp.setOperator(query));
+        System.out.println("Seventh test method");
+    }
+    void eighthTest() {
+    	String query="select * from ipl.csv where id>100 and city='Banglore' ;";
+    	ArrayList<String> al= new ArrayList<String>();
+    	al.add("*");
+    	assertEquals(al,qp.setDesired(query));
+        System.out.println("Eighth test method");
+    }
+    void ninthTest() {
+    	String query="select * from ipl.csv where id>100 and city='Banglore' order by city ;";
+    	ArrayList<String> al= new ArrayList<String>();
+    	al.add("city");
+    	assertEquals(al,qp.setOrderBy(query));
+        System.out.println("Nigth test method");
+    }
+    void tenthTest() {
+    	String query="select * from ipl.csv where id>100 and city='Banglore' order by city group by id ;";
+    	ArrayList<String> al= new ArrayList<String>();
+    	al.add("id");
+    	assertEquals(al,qp.setGroupBy(query));
+        System.out.println("Tenth test method");
+    }
+    void eleventhTest() {
+    	String query="select * sum(id) from ipl.csv where id>100 and city='Banglore' order by city group by id ;";
+    	ArrayList<String> al= new ArrayList<String>();
+    	al.add("sum(id)");
+    	assertEquals(al,qp.setGroupBy(query));
+        System.out.println("Eleventh test method");
+    }
+    
+    
     @AfterEach
     void afterEach() {
         System.out.println("After each test method");
